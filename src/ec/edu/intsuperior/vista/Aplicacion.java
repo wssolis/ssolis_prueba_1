0507df0ab8c3fbc77e9d2b9b7d40161bdc64e790
wssolis <sys.solis@gmail.com>
2022-07-07 20:25:34 -0500

    Evaluacion julio 7

---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Clase1;
+
+/**
+ *
+ * @author Santiago Solis
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        Clase1 obj=new Clase1();
+        obj.metodoInstancia_1();
+    }
+            
+    
+}
